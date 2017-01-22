594950419b8aa5f8a6e884806c826b80495bacbf
petekal1323 <petekal1323@gmail.com>
2017-01-22 13:26:41 -0500

    empty main and song methods

---
+++ BeerSong/src/beersong/BeerSong.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beersong;
+
+/**
+ *
+ * @author Pete
+ */
+public class BeerSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void Ninety_Nine_Bottles_of_Beer(){
+    
+    }
+}
