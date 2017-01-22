e00aa3823088fa2408bafe1513dab01b5e294064
petekal1323 <petekal1323@gmail.com>
2017-01-22 14:16:50 -0500

    This branch has main method calling song method. Working.

--- BeerSong/src/beersong/BeerSong.java
+++ BeerSong/src/beersong/BeerSong.java
@@ -15,10 +15,27 @@ public class BeerSong {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ninety_Nine_Bottles_of_Beer(); //Call the 99 Bottles Method
     }
     
-    public static void Ninety_Nine_Bottles_of_Beer(){
-    
+    public static void Ninety_Nine_Bottles_of_Beer() {
+        int bottles = 99; //Initialize 99 Bottles
+
+        while (bottles > 1) { //While Bottles greater 99
+            System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer"); //More than 99 Bottles
+
+            bottles = bottles - 1; //Decrease Bottles by 1 each time
+
+            if (bottles == 1) {
+                System.out.println("take one down, pass it around, " + bottles + " bottle of beer on the wall."); //1 Bottle Left
+
+            } else {
+                System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall."); //More than 1 Bottle Left
+            }
+
+        }
+
+        System.out.println(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer"); //1 Bottle Left
+        System.out.println("Take one down, pass it around, no more bottles of beer on the wall."); //No more Bottles
     }
 }
